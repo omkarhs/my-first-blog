6ca701317ef0abde353b76a1d1b15614a64b4063
omkarhs <omkarhs22@gmail.com>
2017-04-14 23:15:56 -0400

    blog website underway

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qz^vg92$4ym(2d!ttysyxhqnnwe@=etlcl98qpm6!w%+zaxi*#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'omkarhs.pythonanywhere.com']
 
 
 # Application definition
